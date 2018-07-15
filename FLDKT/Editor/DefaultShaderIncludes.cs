14d1ac53ac0743103f12b63449e7a942594e6a62
Unknown <dotprodukt@gmail.com>
2018-07-15 16:29:13 -0700

    DefaultShaderIncludes.cs

---
+++ FLDKT/Editor/DefaultShaderIncludes.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+
+namespace Unity.FLDKT.Editor
+{
+    static class DefaultShaderIncludes
+    {
+        [ShaderIncludePath]
+        public static string[] GetPaths()
+        {
+            var paths = new string[1];
+            paths[0] = Path.GetFullPath("Packages/com.dotprodukt.unity-fldkt/FLDKT/Shaders/Lib");
+            return paths;
+        }
+    }
+}
